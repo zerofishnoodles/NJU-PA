210bb1af8f0630ea882047ace44a4c7dae47476c
tracer-ics2019 <tracer@njuics.org>
2022-10-11 22:44:37 +0800

    >  run U201714670 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:44:37 up 1 day,  2:22,  1 user,  load average: 0.20, 0.21, 0.35 5ddeabc8427c0045e71a343f66eaa69659f74243

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -2,29 +2,13 @@
 #include <amdev.h>
 #include <nemu.h>
 
-#define KEYDOWN_MASK 0x8000
-
-// size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
-//   switch (reg) {
-//     case _DEVREG_INPUT_KBD: {
-//       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-//       uint32_t key = inl(KBD_ADDR);
-//       kbd->keydown = (uint32_t)(key&0x8000) >> 15;
-//       kbd->keycode = key;
-//       return sizeof(_DEV_INPUT_KBD_t);
-//     }
-//   }
-//   return 0;
-// }
-
-#define KEYDOWN_MASK 0x8000
 
 size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      int key=inl(KBD_ADDR);
-      kbd->keydown = key&KEYDOWN_MASK?1:0;
+      uint32_t key = inl(KBD_ADDR);
+      kbd->keydown = (uint32_t)(key&0x8000) >> 15;
       kbd->keycode = key;
       return sizeof(_DEV_INPUT_KBD_t);
     }
