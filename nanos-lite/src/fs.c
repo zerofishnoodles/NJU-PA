b7b753d495df1fa8556438fac67d041018f1fd52
tracer-ics2019 <tracer@njuics.org>
2022-10-11 22:26:31 +0800

    >  run U201714670 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:26:31 up 1 day,  2:04,  1 user,  load average: 0.54, 0.72, 0.64 d21f5e74ab7ed1c9308cc1d897aca257711e9537

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -71,24 +71,45 @@ size_t fs_read(int fd, void *buf, size_t len) {
   return len;
 }
 
-size_t fs_write(int fd, const void *buf, size_t len) {
-  // boundary
-  if(fd != FD_STDERR && fd != FD_STDIN && fd != FD_STDOUT){
-    if(file_table[fd].open_offset + len >= file_table[fd].size) len = file_table[fd].size - file_table[fd].open_offset;
+// size_t fs_write(int fd, const void *buf, size_t len) {
+//   // boundary
+//   if(fd != FD_STDERR && fd != FD_STDIN && fd != FD_STDOUT){
+//     if(file_table[fd].open_offset + len >= file_table[fd].size) len = file_table[fd].size - file_table[fd].open_offset;
+//   }
+//   assert(len >= 0);
+
+//   size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
+
+//   if(file_table[fd].write != NULL) {
+//     file_table[fd].write(buf, offset, len);
+//   }else {
+//     ramdisk_write(buf, offset, len);
+//   }
+
+//   file_table[fd].open_offset += len;
+//   return len;
+// }
+
+size_t fs_write(int fd, const void *buf, size_t len){
+  size_t length=0;
+  if(fd==1||fd==2){
+    for(int i=0;i<len;i++){
+      _putc(((char *)buf)[i]);
+    }
+    length=len;
   }
-  assert(len >= 0);
-
-  size_t offset = file_table[fd].disk_offset + file_table[fd].open_offset;
-
-  if(file_table[fd].write != NULL) {
-    file_table[fd].write(buf, offset, len);
-  }else {
-    ramdisk_write(buf, offset, len);
+  if(file_table[fd].open_offset+len>=file_table[fd].size)
+    len=file_table[fd].size-file_table[fd].open_offset;
+  if(fd>2){
+    if(file_table[fd].write==NULL)
+      length=ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    else
+      length=file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].open_offset+=length;
   }
-
-  file_table[fd].open_offset += len;
-  return len;
+  return length;
 }
+
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
   switch(whence){
