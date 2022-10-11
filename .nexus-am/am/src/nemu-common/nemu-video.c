7ebc28cec2877f674b15f511ad5e8ff71b52403f
tracer-ics2019 <tracer@njuics.org>
2022-10-11 22:13:04 +0800

    >  run U201714670 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:13:04 up 1 day,  1:50,  1 user,  load average: 0.39, 0.53, 0.42 78294f98abbf7bd6139496232618620454cb5a84

---
+++ .nexus-am/am/src/nemu-common/nemu-video.c
@@ -0,0 +1,46 @@
+#include <am.h>
+#include <amdev.h>
+#include <nemu.h>
+#include <klib.h>
+
+size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
+  switch (reg) {
+    case _DEVREG_VIDEO_INFO: {
+      _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
+      info->width = 400;
+      info->height = 300;
+      return sizeof(_DEV_VIDEO_INFO_t);
+    }
+  }
+  return 0;
+}
+
+size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
+  switch (reg) {
+    case _DEVREG_VIDEO_FBCTL: {
+      _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+      int x=ctl->x,y=ctl->y,h=ctl->h,w=ctl->w;
+      int W=screen_width();
+      for(int cur_y = 0; cur_y < h; cur_y++) {
+        for(int cur_x = 0; cur_x < w; cur_x++) {
+          uint32_t addr = FB_ADDR + (((y + cur_y) * W) + x + cur_x) * 4;
+          uint32_t data = ctl->pixels[cur_y * w + cur_x];
+          outl(addr, data);
+        }
+      }
+      if (ctl->sync) {
+        outl(SYNC_ADDR, 0);
+      }
+      return size;
+    }
+  }
+  return 0;
+}
+
+void __am_vga_init() {
+  int i;
+  int size = screen_width() * screen_height();
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < size; i ++) fb[i] = i;
+  draw_sync();
+}
---
+++ .nexus-am/libs/klib/src/string.c
@@ -0,0 +1,83 @@
+#include "klib.h"
+
+#if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+size_t strlen(const char *s) {
+    int i = 0;
+    for(;s[i];i++){}
+    return i;
+}
+
+char *strcpy(char* dst,const char* src) {
+    int i = 0;
+    while(src[i]){
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = 0;
+  return dst;
+}
+
+char* strncpy(char* dst, const char* src, size_t n) {
+    int i = 0;
+    while(src[i] && (i<n)){
+        dst[i] = src[i];
+        i++;
+    }
+    while(i<n){
+        dst[i] = 0;
+        i++;
+    }
+  return dst;
+}
+
+char* strcat(char* dst, const char* src) {
+    int i = strlen(dst);
+    strcpy((char*)(dst+i), src);
+    return dst;
+}
+
+int strcmp(const char* s1, const char* s2) {
+    int i = 0;
+    for(;s1[i];i++){
+        if(s1[i]!=s2[i])
+            break;
+    }
+  return (s1[i]-s2[i]);
+}
+
+int strncmp(const char* s1, const char* s2, size_t n) {
+    int i = 0;
+    for(;s1[i] && (i<n);i++){
+        if(s1[i]!=s2[i])
+            break;
+    }
+    if(i==n)
+        return 0;
+    return (s1[i]-s2[i]);
+}
+
+void* memset(void* v,int c,size_t n) {
+    for(int i = 0; i < n;i++){
+        ((char *)v)[i] = (char)c;
+    }
+    return v;
+}
+
+void* memcpy(void* out, const void* in, size_t n) {
+    for(int i = 0; i < n; i++){
+        ((char*)out)[i] = ((char*)in)[i];
+    }
+    return out;
+}
+
+int memcmp(const void* s1, const void* s2, size_t n){
+    for(int i = 0; i < n; i++){
+        if(((char*)s1)[i] != ((char*)s2)[i]){
+            return ((char*)s1)[i] - ((char*)s2)[i];
+        }
+    }
+    return 0;
+}
+
+#endif
