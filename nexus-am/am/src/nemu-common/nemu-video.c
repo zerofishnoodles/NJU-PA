a4ac4660b5702e95e48213079442df7cbd186769
tracer-ics2019 <tracer@njuics.org>
2022-10-11 22:44:55 +0800

    >  run U201714670 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:44:55 up 1 day,  2:22,  1 user,  load average: 0.22, 0.21, 0.34 50217cbe913615fa9efa78ecbe3c69d4def9ac20

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,57 +1,7 @@
-// #include <am.h>
-// #include <amdev.h>
-// #include <nemu.h>
-// #include <klib.h>
-
-// size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
-//   switch (reg) {
-//     case _DEVREG_VIDEO_INFO: {
-//       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-//       info->width = 400;
-//       info->height = 300;
-//       return sizeof(_DEV_VIDEO_INFO_t);
-//     }
-//   }
-//   return 0;
-// }
-
-// size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
-//   switch (reg) {
-//     case _DEVREG_VIDEO_FBCTL: {
-//       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-//       int x=ctl->x,y=ctl->y,h=ctl->h,w=ctl->w;
-//       int W=screen_width();
-//       for(int cur_y = 0; cur_y < h; cur_y++) {
-//         for(int cur_x = 0; cur_x < w; cur_x++) {
-//           uint32_t addr = FB_ADDR + (((y + cur_y) * W) + x + cur_x) * 4;
-//           uint32_t data = ctl->pixels[cur_y * w + cur_x];
-//           outl(addr, data);
-//         }
-//       }
-//       if (ctl->sync) {
-//         outl(SYNC_ADDR, 0);
-//       }
-//       return size;
-//     }
-//   }
-//   return 0;
-// }
-
-// void __am_vga_init() {
-//   int i;
-//   int size = screen_width() * screen_height();
-//   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-//   for (i = 0; i < size; i ++) fb[i] = i;
-//   draw_sync();
-// }
-
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
-
-void draw_sync();
-int screen_width();
-int screen_height();
+#include <klib.h>
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -71,12 +21,11 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
       int x=ctl->x,y=ctl->y,h=ctl->h,w=ctl->w;
       int W=screen_width();
-      int H=screen_height();
-      uint32_t *pixels=ctl->pixels;
-      uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
-      for(int i=0;i<h;i++){
-        for(int j=0;j<w;j++){
-          fb[(y+i)*W+x+j]=pixels[i*w+j];
+      for(int cur_y = 0; cur_y < h; cur_y++) {
+        for(int cur_x = 0; cur_x < w; cur_x++) {
+          uint32_t addr = FB_ADDR + (((y + cur_y) * W) + x + cur_x) * 4;
+          uint32_t data = ctl->pixels[cur_y * w + cur_x];
+          outl(addr, data);
         }
       }
       if (ctl->sync) {
@@ -92,6 +41,7 @@ void __am_vga_init() {
   int i;
   int size = screen_width() * screen_height();
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < size; i ++) fb[i] = 0;
+  for (i = 0; i < size; i ++) fb[i] = i;
   draw_sync();
 }
+
